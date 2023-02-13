ba0d68c1fa91ce088d848917245d084a2c109352
Alvin Mithamo <alvinmithamo100@gmail.com>
2023-02-13 13:38:50 +0300

    completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
+class Book
+    attr_accessor : title, :author, :page_count, :genre
 
+    def initialize(title="My Journey", author="Awiti Cole", page_count="300", genre="Motivational")
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
+    end
+    def turn_page
+        puts "Wow! you read fast!"
+    end
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor : brand, :color, :size, :material, :conditon
+
+    def initialize(brand="Nike", color="white", size="7", material="leather", condition="usable")
+        @brand = brand
+        @color = color
+        @size = size
+        @material = material
+        @condition = condition
+    end
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
